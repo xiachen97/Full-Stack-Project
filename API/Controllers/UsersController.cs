5d0ddb9b9303f2083323b8031781cd64163209ec
xiachen97 <xiachen2@oakland.edu>
2024-09-26 19:14:13 -0400

    Update UsersController.cs

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -21,7 +21,7 @@ namespace API.controllers
         }
 
         //api/users/3
-        [HttpGet("{id}")]
+        [HttpGet("{id
         public async Task<ActionResult<AppUser>> GetUser(int id){
             return await _context.Users.FindAsync(id); 
         }
